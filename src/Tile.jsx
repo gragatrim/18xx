c8aa655b5e980e46086b49fdd3cbcd7d67e78548
Gragatrim <gragatrim@localhost.localdomain>
2018-11-04 20:49:59 -0500

    Keep station markers on the hex as it upgrades

--- src/Tile.jsx
+++ src/Tile.jsx
@@ -15,7 +15,14 @@ const Tile = ({ id, border, transparent, onClick, translateX, translateY, hex, r
   if(!newHex && !hex) {
     return null;
   }
-  let finalHex = R.merge(hex, newHex);
+
+  let tempHex = R.merge(hex, newHex);
+  //This ensures that we keep any tokens in the city as it upgrades
+  let finalHex = R.mergeDeepRight(tempHex, {cities: hex.cities});
+  if (!R.isNil(newHex.cities) && !R.isNil(newHex.cities[0].size)) {
+    //This ensures that the number of city spots is correct for the new tile we're updating to
+    finalHex.cities[0].size = newHex.cities[0].size;
+  }
   return <Hex hex={finalHex} id={id} border={border} onClick={onClick} transparent={transparent} translateX={translateX} translateY={translateY} rotation={rotation} />;
 };
 
